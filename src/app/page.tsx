936f483cb5ea40e39abbd2cfb1ee9a61d7c10499
Amir Alam <sharpprogrammer2018@gmail.com>
2024-10-02 18:57:23 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
         {!wallet ? (
           <TonConnectButton className="ton-connect-page__button" />
         ) : (
-          <Button onClick={() => disconnectWallet()} className="btn">
+          <Button onClick={() => disconnectWallet()} size="s">
             Disconnect Wallet
           </Button>
         )}
