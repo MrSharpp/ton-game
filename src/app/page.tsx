22928ce53efee7219ff56aeb722433a8235ac2bd
Amir Alam <sharpprogrammer2018@gmail.com>
2024-10-02 18:05:04 +0530

    add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,78 @@
-'use client';
+"use client";
 
-import { Section, Cell, Image, List, Button } from '@telegram-apps/telegram-ui';
+import { Section, Cell, Image, List, Button } from "@telegram-apps/telegram-ui";
 
-import { Link } from '@/components/Link/Link';
+import { Link } from "@/components/Link/Link";
 
-import tonSvg from './_assets/ton.svg';
-import { TonConnectButton, useTonConnectUI, useTonWallet } from '@tonconnect/ui-react';
-import { useUtils } from '@telegram-apps/sdk-react';
-
+import tonSvg from "./_assets/ton.svg";
+import {
+  TonConnectButton,
+  useTonConnectUI,
+  useTonWallet,
+} from "@tonconnect/ui-react";
+import { useUtils } from "@telegram-apps/sdk-react";
 
 export default function Home() {
   const wallet = useTonWallet();
   const utils = useUtils();
   const [tonConnectUI, setOptions] = useTonConnectUI();
 
-  function sendToOwnerAddress(){
+  function sendToOwnerAddress() {
     console.log("TDA");
-    
+
     tonConnectUI.sendTransaction({
       messages: [
         {
           address: "0QCEUg8zL-iW1cQW36NZ6ngw9NXDI9AWjX5Bsebvy2-kcsNL", // destination address
-          amount: "1000000000" //Toncoin in nanotons
-        }
+          amount: "1000000000", //Toncoin in nanotons
+        },
       ],
       validUntil: Math.floor(Date.now() / 1000) + 60,
-    })
+    });
   }
 
-  function copyToClipBoard(){
-    utils.shareURL("https://t.me/shahzar_2024_bot")
+  function copyToClipBoard() {
+    utils.shareURL("https://t.me/shahzar_2024_bot");
   }
 
-  function disconnectWallet(){
-    tonConnectUI.disconnect()
+  function disconnectWallet() {
+    tonConnectUI.disconnect();
   }
 
   return (
-    <div style={{backgroundColor: 'black', textAlign: 'center', paddingTop: '50%', height: '100%'}}>
-      <h1 style={{marginBottom: '50px'}}>Amir Mini App</h1>
-       <div style={{flexDirection: 'column', display: 'flex', gap: '50px', flexBasis: 'auto'}}>
-       {!wallet ? (<TonConnectButton className='ton-connect-page__button'/>) : (<Button onClick={() => disconnectWallet()} className='btn'>
-              Disconnect Wallet
-        </Button>)}
-        {!!wallet && (<Button   onClick={() => sendToOwnerAddress()} className='btn'>
-         
+    <div
+      style={{
+        backgroundColor: "black",
+        textAlign: "center",
+        paddingTop: "50%",
+        height: "100%",
+      }}
+    >
+      <h1 style={{ marginBottom: "50px" }}>Amir Mini App</h1>
+      <div
+        style={{
+          flexDirection: "column",
+          display: "flex",
+          gap: "50px",
+          flexBasis: "auto",
+        }}
+      >
+        {!wallet ? (
+          <TonConnectButton className="ton-connect-page__button" />
+        ) : (
+          <Button onClick={() => disconnectWallet()} className="btn">
+            Disconnect Wallet
+          </Button>
+        )}
+        {!!wallet && (
+          <Button onClick={() => sendToOwnerAddress()} className="btn">
             Make A Small Transaction (1 Ton)
-        </Button>)}
-        <Button onClick={() => copyToClipBoard()} className='btn'>
-          
-           Refferals
+          </Button>
+        )}
+        <Button onClick={() => copyToClipBoard()} className="btn">
+          Refferals
         </Button>
-       </div>
-   
+      </div>
     </div>
   );
 }
