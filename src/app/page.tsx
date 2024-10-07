1c650b0e61dd66203ed382811cb4461493cf755d
Amir Alam <sharpprogrammer2018@gmail.com>
2024-10-07 21:56:42 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { useUtils } from "@telegram-apps/sdk-react";
+import { Caption, Subheadline } from "@telegram-apps/telegram-ui";
+
 export default function Home() {
+  const util = useUtils();
+
   return (
     <div
       style={{
@@ -18,6 +23,44 @@ export default function Home() {
       }}
     >
       <img src="/banner.png" alt="TON Connect" width={300} height={300} />
+
+      <Subheadline level="1" weight="3">
+        Time streak Task: 0
+      </Subheadline>
+
+      <Subheadline level="1" weight="3">
+        Fren streak Task: 0
+      </Subheadline>
+
+      <div className="mt-10">
+        <Subheadline level="1" weight="3">
+          Social Tasks
+        </Subheadline>
+
+        <div className="flex flex-col">
+          <Caption
+            level="1"
+            weight="3"
+            onClick={() => util.openLink("https://www.instagram.com")}
+          >
+            Instagram
+          </Caption>
+          <Caption
+            level="1"
+            weight="3"
+            onClick={() => util.openLink("https://www.x.com")}
+          >
+            Twitter
+          </Caption>
+          <Caption
+            level="1"
+            weight="3"
+            onClick={() => util.openLink("https://www.telegram.com")}
+          >
+            Telegram
+          </Caption>
+        </div>
+      </div>
     </div>
   );
 }
