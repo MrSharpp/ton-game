2c9d50eb1c3c9a7ec13aa10bfd6516909b5b9bf4
Mohd Ahmad <61076339+MohdAhmad1@users.noreply.github.com>
2024-10-08 12:03:19 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,15 @@ import { Caption, Subheadline } from "@telegram-apps/telegram-ui";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen">
-      <video
-        loop
-        playsInline
-        muted
-        autoPlay
-        className="w-full h-screen object-cover"
-      >
-        <source src={"/splash.mp4"} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+    <video
+      loop
+      playsInline
+      muted
+      autoPlay
+      className="h-full w-full h-inherit object-cover"
+    >
+      <source src={"/splash.mp4"} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
   );
 }
