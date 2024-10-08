092d3f82e7c080b228851006b07841f06e264499
Mohd Ahmad <61076339+MohdAhmad1@users.noreply.github.com>
2024-10-08 16:41:09 +0530

    Update user-provider.tsx

--- src/app/user-provider.tsx
+++ src/app/user-provider.tsx
@@ -26,6 +26,8 @@ export function UserProvider({ children }: any) {
 
   const startParam = useLaunchParams().startParam;
 
+  console.log({ startParam });
+
   function fetchUser() {
     return fetch(`/api/users/upsert?referId=${startParam}`, {
       method: "POST",
