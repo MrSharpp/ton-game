7022fe0128c62d81cee7f4f9d79b72579ef2d2bd
Mohd Ahmad <61076339+MohdAhmad1@users.noreply.github.com>
2024-10-08 16:39:09 +0530

    update

--- src/app/api/friends/[userId]/route.ts
+++ src/app/api/friends/[userId]/route.ts
@@ -1,6 +1,10 @@
 import { prismaClient } from "@/db/prisma-client";
 import { NextResponse } from "next/server";
 
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
+
 export async function GET(request: Request, { params }: any) {
   const friends = await prismaClient.friend.findMany({
     where: {
