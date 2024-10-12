3d55f581f848d23a96034931d65ad85bd4210c99
Amir Alam <sharpprogrammer2018@gmail.com>
2024-10-13 01:29:35 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,9 +2,9 @@ export const BOT_USERNAME = "AiBullsIO_bot/AiBulls";
 export const FRIENDS_STREAK_THRESHHOLD = 3;
 export const COUNT_OF_TASKS = 3;
 export const SHARE_MESSAGE = `
-%0A•The most distinguished Airdrop in the history of TG mini apps.
-%0A•Boost your allocation with streak tasks.
-%0A•Join now.`;
+%0A•The most distinguished Airdrop in the history of TG mini apps.🎮
+%0A•Boost your allocation with streak tasks.💰
+%0A•Join now.🔥`;
 
 export const TIME_TASK_ADDITION_NUMBER = 100;
 export const FEN_TASK_ADDITION_NUMBER = 1000;
